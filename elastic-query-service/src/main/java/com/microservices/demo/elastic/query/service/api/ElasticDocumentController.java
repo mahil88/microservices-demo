1ab4e417cc176a6710734a5323cbce98f240f079
mahil88 <mahil.j@affiniti.biz>
2022-12-16 11:35:38 +0530

    oauth + keycloak

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -13,13 +13,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
-import java.util.ArrayList;
 import java.util.List;
 
+@PreAuthorize("isAuthenticated()")
 @RestController
 @RequestMapping(value = "/documents", produces = "application/vnd.api.v1+json")
 public class ElasticDocumentController {
@@ -34,17 +36,18 @@ public class ElasticDocumentController {
     @Value("${server.port}")
     private String port;
 
+    @PostAuthorize("hasPermission(returnObject, 'READ')")
     @Operation(summary = "Get all elastic documents.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful response.", content = {
                     @Content(mediaType = "application/vnd.api.v1+json",
-                            schema = @Schema(implementation = ElasticQueryServiceResponseModel.class)
+                             schema = @Schema(implementation = ElasticQueryServiceResponseModel.class)
                     )
             }),
             @ApiResponse(responseCode = "400", description = "Not found."),
             @ApiResponse(responseCode = "500", description = "Internal server error.")
     })
-    @GetMapping
+    @GetMapping("")
     public @ResponseBody
     ResponseEntity<List<ElasticQueryServiceResponseModel>> getAllDocuments() {
         List<ElasticQueryServiceResponseModel> response = elasticQueryService.getAllDocuments();
@@ -52,6 +55,8 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(response);
     }
 
+
+    @PreAuthorize("hasPermission(#id, 'ElasticQueryServiceResponseModel','READ')")
     @Operation(summary = "Get elastic document by id.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful response.", content = {
@@ -66,9 +71,8 @@ public class ElasticDocumentController {
     public @ResponseBody
     ResponseEntity<ElasticQueryServiceResponseModel>
     getDocumentById(@PathVariable @NotEmpty String id) {
-        ElasticQueryServiceResponseModel elasticQueryServiceResponseModel =
-                elasticQueryService.getDocumentById(id);
-        LOG.debug("Elasticsearch returned document with id {}", id);
+        ElasticQueryServiceResponseModel elasticQueryServiceResponseModel = elasticQueryService.getDocumentById(id);
+        LOG.debug("Elasticsearch returned document with id {} on port {}", id, port);
         return ResponseEntity.ok(elasticQueryServiceResponseModel);
     }
 
@@ -88,10 +92,13 @@ public class ElasticDocumentController {
     getDocumentByIdV2(@PathVariable @NotEmpty String id) {
         ElasticQueryServiceResponseModel elasticQueryServiceResponseModel = elasticQueryService.getDocumentById(id);
         ElasticQueryServiceResponseModelV2 responseModelV2 = getV2Model(elasticQueryServiceResponseModel);
-        LOG.debug("Elasticsearch returned document with id {}", id);
+        LOG.debug("Elasticsearch returned document with id {} on port {}", id, port);
         return ResponseEntity.ok(responseModelV2);
     }
 
+
+    @PreAuthorize("hasRole('APP_USER_ROLE') || hasRole('APP_SUPER_USER_ROLE') || hasAuthority('SCOPE_APP_USER_ROLE')")
+    @PostAuthorize("hasPermission(returnObject, 'READ')")
     @Operation(summary = "Get elastic document by text.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful response.", content = {
---
+++ elastic-query-service/src/main/resources/init-data.sql
@@ -0,0 +1,37 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+INSERT INTO public.users(
+	id, username, firstname, lastname)
+	VALUES ('0f4884c8-584b-4893-9ad7-a93a6edca870', 'app_user', 'Standard', 'User');
+INSERT INTO public.users(
+	id, username, firstname, lastname)
+	VALUES ('7631a49e-83b5-4a4a-901b-9d9af7512558', 'app_admin', 'Admin', 'User');
+INSERT INTO public.users(
+	id, username, firstname, lastname)
+	VALUES ('51ef05fc-e4eb-4253-9d2e-5ba04cb59b3e', 'app_super_user', 'Super', 'User');
+
+
+insert into documents(id, document_id)
+values ('c1df7d01-4bd7-40b6-86da-7e2ffabf37f7', 1);
+insert into documents(id, document_id)
+values ('f2b2d644-3a08-4acb-ae07-20569f6f2a01', 2);
+insert into documents(id, document_id)
+values ('90573d2b-9a5d-409e-bbb6-b94189709a19', 3);
+
+insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
+values (uuid_generate_v4(),'0f4884c8-584b-4893-9ad7-a93a6edca870', 'c1df7d01-4bd7-40b6-86da-7e2ffabf37f7', 'READ');
+
+insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
+values (uuid_generate_v4(),'7631a49e-83b5-4a4a-901b-9d9af7512558', 'c1df7d01-4bd7-40b6-86da-7e2ffabf37f7', 'READ');
+
+insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
+values (uuid_generate_v4(),'7631a49e-83b5-4a4a-901b-9d9af7512558', 'f2b2d644-3a08-4acb-ae07-20569f6f2a01', 'READ');
+
+insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
+values (uuid_generate_v4(), '7631a49e-83b5-4a4a-901b-9d9af7512558', '90573d2b-9a5d-409e-bbb6-b94189709a19', 'READ');
+
+insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
+values (uuid_generate_v4(), '51ef05fc-e4eb-4253-9d2e-5ba04cb59b3e', 'c1df7d01-4bd7-40b6-86da-7e2ffabf37f7', 'READ');
+
+
